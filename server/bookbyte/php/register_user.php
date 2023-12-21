06efd64aad6e75929ef254fe9cf2c70cee40f31c
nurafifah0 <nurafifahsupyan@gmail.com>
2023-12-22 00:48:45 +0800

    server updated

--- server/bookbyte/php/register_user.php
+++ server/bookbyte/php/register_user.php
@@ -12,6 +12,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $password = sha1($_POST['password']);
 
+//INSERT INTO `tbl_users`( `user_email`, `user_name`, `user_password`) VALUES ('[value-1]','[value-2]','[value-3]')
 $sqlinsert = "INSERT INTO `tbl_users`(`user_email`, `user_name`, `user_password`) VALUES ('$email','$name','$password')";
 
 if ($conn->query($sqlinsert) === TRUE) {
